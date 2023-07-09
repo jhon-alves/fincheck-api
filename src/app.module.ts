87645337b992f70a593a36f4f3d5807afb7e4ab2
Jonatas Alves <jonatas.a2012@hotmail.com>
2023-07-09 02:59:36 -0300

    added imports categories, bank accounts and transactions modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { AuthGuard } from './modules/auth/auth.guard';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { BankAccountsModule } from './modules/bank-accounts/bank-accounts.module';
+import { TransactionsModule } from './modules/transactions/transactions.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule],
+  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule, BankAccountsModule, TransactionsModule],
   controllers: [],
   providers: [
     {
