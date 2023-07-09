4adfb64e3bb5adf0d3a67bbd467424c4a3c39495
Jonatas Alves <jonatas.a2012@hotmail.com>
2023-07-09 03:04:14 -0300

    changed send attributes

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -5,14 +5,13 @@ import { UsersRepository } from 'src/shared/database/repositories/users.reposito
 export class UsersService {
   constructor(private readonly usersRepo: UsersRepository) {}
 
-  async getUserById(userId: string) {
-    const user = await this.usersRepo.findUnique({
+  getUserById(userId: string) {
+    return this.usersRepo.findUnique({
       where: { id: userId },
+      select: {
+        name: true,
+        email: true
+      }
     });
-
-    return {
-      name: user.name,
-      email: user.email
-    }
   }
 }
